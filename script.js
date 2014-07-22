cec4b6d6c5218bb8863221113a27e8e8ce282342
Shane Todhunter <shane@14oranges.com>
2014-07-21 21:38:32 -0700

    d

--- script.js
+++ script.js
@@ -57,7 +57,7 @@
     //
     function onSuccess(position) {
     //alert('here');
-        $scope.geoMessage = 'Latitude: '           + position.coords.latitude              + '<br />' +
+        var geoLocationString = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
                             'Altitude: '           + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
@@ -65,7 +65,7 @@
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
                             'Timestamp: '          + position.timestamp          + '<br />';
-    	alert $scope.geoMessage;
+    	alert geoLocationString;
     }
 
     // onError Callback receives a PositionError object
